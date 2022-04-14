fd92681b8c9656e1308d267f06cd09683b455c5c
YunYouJun <me@yunyoujun.cn>
2022-04-14 21:30:19 +0800

    feat: add gtm

---
+++ src/modules/gtm.ts
@@ -0,0 +1,7 @@
+import { createGtm } from '@gtm-support/vue-gtm'
+import type { UserModule } from '~/types'
+
+export const install: UserModule = ({ app }) => {
+  // add google tag manager, and add GA4 in gtag
+  app.use(createGtm({ id: 'GTM-5FJSV46' }))
+}
