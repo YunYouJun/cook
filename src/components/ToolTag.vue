f1353054aee308fe8cc27522541146d6ad9a2522
YunYouJun <me@yunyoujun.cn>
2022-04-16 01:16:58 +0800

    fix: highlight tool border when dark

--- src/components/ToolTag.vue
+++ src/components/ToolTag.vue
@@ -7,7 +7,7 @@ defineProps<{
 <template>
   <span
     class="tag rounded" p="x-2"
-    border="~ stone-200 dark:stone-800"
+    :border="'~ stone-200 dark:stone-800' + (active ? 'dark:stone-200' : '')"
     :bg="active ? 'stone-500 opacity-90' : 'stone-300 opacity-20'"
     :text="active ? 'stone-100' : 'stone-800 dark:stone-200'"
   >
