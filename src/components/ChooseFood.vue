22051318d1a5b35fb205d60fcb28dff74850feef
YunYouJun <me@yunyoujun.cn>
2022-04-15 23:01:54 +0800

    fix: multi tools

--- src/components/ChooseFood.vue
+++ src/components/ChooseFood.vue
@@ -1,5 +1,4 @@
 <script lang="ts" setup>
-import { storeToRefs } from 'pinia'
 import { useGtm } from '@gtm-support/vue-gtm'
 import MeatTag from './MeatTag.vue'
 import StapleTag from './StapleTag.vue'
