bc87ed50f3c8967dda314ae9be4fa5630ac2ac40
YunYouJun <me@yunyoujun.cn>
2022-04-16 23:14:48 +0800

    chore: update title by command

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,13 @@
 <script setup lang="ts">
 // https://github.com/vueuse/head
 // you can use this to manipulate the document head in any components,
-import { useWeixin } from './composables/weixin'
-
 // they will be rendered correctly in the html results with vite-ssg
 useHead({
-  title: '好的，今天我们来做菜！',
+  title: '隔离食用手册',
   meta: [
     { name: 'description', content: '好的，今天我们来做菜！' },
   ],
 })
-
-useWeixin()
 </script>
 
 <template>
