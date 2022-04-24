14f495df87f2a79b149f4fa0a7c2cf6c21acabf6
bryce <1907004005@gmail.com>
2022-04-24 23:38:35 +0800

    feat(theme-color): toggle theme color along with dark mode using useHead

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <script setup lang="ts">
-import { useThemeColor } from './composables/theme-color'
 import { isDark } from '~/composables'
-
 // https://github.com/vueuse/head
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
 useHead({
   title: '隔离食用手册',
   meta: [
-    { name: 'description', content: '好的，今天我们来做菜！' },
+    {
+      name: 'description',
+      content: '好的，今天我们来做菜！',
+    },
+    {
+      name: 'theme-color',
+      content: computed(() => isDark.value ? '#121212' : '#fff'),
+    },
   ],
 })
-
-useThemeColor(isDark)
 </script>
 
 <template>
