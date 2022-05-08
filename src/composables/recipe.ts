ebc6e2d037a99f7cf9112e88ab4e5ab6d6066441
YunYouJun <me@yunyoujun.cn>
2022-05-09 02:51:29 +0800

    refactor: add useRecipe

---
+++ src/composables/recipe.ts
@@ -0,0 +1,78 @@
+import type { Ref } from 'vue'
+import { storeToRefs } from 'pinia'
+import { useGtm } from '@gtm-support/vue-gtm'
+import type { Recipe } from '~/types'
+
+import { useRecipeStore } from '~/stores/recipe'
+import type { StuffItem } from '~/data/food'
+
+export function useRecipe(recipe: Ref<Recipe>) {
+  const gtm = useGtm()
+
+  const rStore = useRecipeStore()
+  const { curMode, curTool } = storeToRefs(rStore)
+  const curStuff = computed(() => rStore.selectedStuff)
+
+  // 默认严格模式
+  const displayedRecipe = computed(() => {
+    if (curMode.value === 'strict') {
+      return recipe.value.filter((item) => {
+        const stuffFlag = curStuff.value.every(stuff => item.stuff.includes(stuff))
+        const toolFlag = item.tools?.includes(curTool.value)
+        return curTool.value ? stuffFlag && toolFlag : stuffFlag
+      })
+    }
+    else if (curMode.value === 'loose') {
+      return recipe.value.filter((item) => {
+        const stuffFlag = curStuff.value.some(stuff => item.stuff.includes(stuff))
+        const toolFlag = item.tools?.includes(curTool.value)
+
+        // 同时存在 厨具和材料，则同时判断
+        if (curTool.value && curStuff.value.length) {
+          return stuffFlag && toolFlag
+        }
+        else {
+          if (curStuff.value.length)
+            return stuffFlag
+          else if (curTool.value)
+            return toolFlag
+
+          return false
+        }
+      })
+    }
+    // survival
+    else {
+      return recipe.value.filter((item) => {
+        const stuffFlag = item.stuff.every(stuff => curStuff.value.includes(stuff))
+        const toolFlag = item.tools?.includes(curTool.value)
+        return curTool.value ? stuffFlag && toolFlag : stuffFlag
+      })
+    }
+  })
+
+  /**
+   * toggle tool
+   * @param item
+   */
+  const clickTool = (item: StuffItem) => {
+    const value = item.name
+    rStore.toggleTools(value)
+
+    gtm?.trackEvent({
+      event: 'click',
+      category: `tool_${value}`,
+      action: 'click_tool',
+      label: '工具',
+    })
+    gtm?.trackEvent({
+      event: 'click_tool',
+      action: item.name,
+    })
+  }
+
+  return {
+    displayedRecipe,
+    clickTool,
+  }
+}
