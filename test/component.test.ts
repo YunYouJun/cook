d7b6fff88835b2f542da57c3b6da01206ba6e23e
YunYouJun <me@yunyoujun.cn>
2022-06-27 09:44:49 +0800

    chore: trigger vercel

--- test/component.test.ts
+++ test/component.test.ts
@@ -1,10 +1,11 @@
 // import { mount } from '@vue/test-utils'
 // import { describe, expect, it, vi } from 'vitest'
+import { describe } from 'vitest'
 // import { createTestingPinia } from '@pinia/testing'
 // import ChooseFood from '../src/components/ChooseFood.vue'
 // import { useRecipeStore } from '~/stores/recipe'
 
-// describe('ChooseFood.vue', () => {
+describe('ChooseFood.vue', () => {
 //   it('should render', async () => {
 //     const pinia = createTestingPinia({
 //       createSpy: vi.fn,
@@ -17,43 +18,43 @@
 //       },
 //     })
 
-//     const rStore = useRecipeStore()
+  //     const rStore = useRecipeStore()
 
-//     rStore.reset()
-//     rStore.addStuff('黄瓜')
-//     rStore.addStuff('黄瓜')
+  //     rStore.reset()
+  //     rStore.addStuff('黄瓜')
+  //     rStore.addStuff('黄瓜')
 
-//     expect(rStore.selectedStuff).toEqual(['黄瓜'])
+  //     expect(rStore.selectedStuff).toEqual(['黄瓜'])
 
-//     expect(wrapper.find('.vegetable-tag').exists()).toBe(true)
-//     expect(wrapper.find('.cook-filter-recipes').exists()).toBe(true)
+  //     expect(wrapper.find('.vegetable-tag').exists()).toBe(true)
+  //     expect(wrapper.find('.cook-filter-recipes').exists()).toBe(true)
 
-//     await wrapper.find('.vegetable-tag').trigger('click')
+  //     await wrapper.find('.vegetable-tag').trigger('click')
 
-//     const tags = wrapper.find('.cook-filter-recipes').findAll('.dish-tag')
+  //     const tags = wrapper.find('.cook-filter-recipes').findAll('.dish-tag')
 
-//     expect(tags.length > 0).toBe(true)
+  //     expect(tags.length > 0).toBe(true)
 
-//     tags.forEach((tag) => {
-//       const result = tag.text().includes('🥒') || tag.text().includes('🍲')
-//       expect(result).toBe(true)
-//     })
-//   })
+  //     tags.forEach((tag) => {
+  //       const result = tag.text().includes('🥒') || tag.text().includes('🍲')
+  //       expect(result).toBe(true)
+  //     })
+  //   })
 
-//   it('should be interactive', async () => {
-//     // const wrapper = mount(ChooseFood)
+  //   it('should be interactive', async () => {
+  //     // const wrapper = mount(ChooseFood)
+  //     // expect(wrapper.text()).toContain('0')
+
+  //     // expect(wrapper.find('.inc').exists()).toBe(true)
+
+  //     // expect(wrapper.find('.dec').exists()).toBe(true)
+
+  //     // await wrapper.get('.inc').trigger('click')
+
+  //     // expect(wrapper.text()).toContain('1')
+
+  //     // await wrapper.get('.dec').trigger('click')
+
 //     // expect(wrapper.text()).toContain('0')
-
-//     // expect(wrapper.find('.inc').exists()).toBe(true)
-
-//     // expect(wrapper.find('.dec').exists()).toBe(true)
-
-//     // await wrapper.get('.inc').trigger('click')
-
-//     // expect(wrapper.text()).toContain('1')
-
-//     // await wrapper.get('.dec').trigger('click')
-
-//     // expect(wrapper.text()).toContain('0')
 //   })
-// })
+})
