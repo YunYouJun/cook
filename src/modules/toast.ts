1f5d2802417ccce9895d81a15f6fc31f00c7a925
YunYouJun <me@yunyoujun.cn>
2022-12-19 06:23:18 +0800

    chore: fix lint

--- src/modules/toast.ts
+++ src/modules/toast.ts
@@ -3,7 +3,7 @@ import type { UserModule } from '~/types'
 
 // import 'vue-toastification/dist/index.css'
 
-export const install: UserModule = ({ app }) => {
+export const install: UserModule = (_ctx) => {
   // add google tag manager, and add GA4 in gtag
   // app.use(Toast)
 }
