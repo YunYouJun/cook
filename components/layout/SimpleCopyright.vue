85c8f4fcd12324e0fa281cb56b13b7e152e6d880
YunYouJun <me@yunyoujun.cn>
2023-11-06 01:46:37 +0800

    chore: update simple copyright

--- components/layout/SimpleCopyright.vue
+++ components/layout/SimpleCopyright.vue
@@ -1,13 +1,22 @@
 <template>
-  <div text="center sm" my-2>
+  <div text="center sm" my-3>
     <CurrentVersion />
-    <a
-      href="https://www.bilibili.com/opus/649847454294868008" target="_blank"
-      class="inline-flex items-center justify-center"
-    >
-      <span>Made</span>
-      <div i-ri-bilibili-line mx-1 class="text-pink-400" />
-      <span>云游君</span>
-    </a>
+    <div flex="~" justify-center items-center gap="2">
+      <a
+        href="https://github.com/YunYouJun/cook" target="_blank"
+        class="inline-flex items-center justify-center"
+      >
+        <div i-ri-github-line mr-1 />
+        <span>Code</span>
+      </a>
+      by
+      <a
+        href="https://www.bilibili.com/opus/649847454294868008" target="_blank"
+        class="inline-flex items-center justify-center"
+      >
+        <div i-ri-bilibili-line mr-1 class="text-pink-400" />
+        <span>云游君</span>
+      </a>
+    </div>
   </div>
 </template>
