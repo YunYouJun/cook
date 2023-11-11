44f9b40ee5e24406aa0bb14b547bb85143fcad8c
YunYouJun <me@yunyoujun.cn>
2023-11-11 19:51:51 +0800

    feat: add 麻婆豆腐, close #55

--- constants/index.ts
+++ constants/index.ts
@@ -2,6 +2,6 @@ export const appName = '食用手册'
 export const appDescription = '好的，今天我们来做菜！'
 
 export const namespace = 'cook'
-export const lastDbUpdated = '2023-11-11 19:46:02'
+export const lastDbUpdated = '2023-11-11 19:51:02'
 
 export * from './links'
