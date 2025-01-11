78fa350ced7b09099701100845bd4e85f223413d
George Chen <cjzjzzz@gmail.com>
2025-01-12 01:35:19 +0800

    feat: use <button> to improve a11y and keyboard navigation experience (#74)

--- app/components/tags/VegetableTag.vue
+++ app/components/tags/VegetableTag.vue
@@ -5,12 +5,12 @@ defineProps<{
 </script>
 
 <template>
-  <span
+  <button
     class="vegetable-tag rounded tag" p="x-2"
     border="~ green-200 dark:green-800"
     :bg="active ? 'green-600 opacity-90' : 'green-300 opacity-20'"
     :text="active ? 'green-100' : 'green-800 dark:green-200'"
   >
     <slot />
-  </span>
+  </button>
 </template>
--- app/pages/recipes/history.vue
+++ app/pages/recipes/history.vue
@@ -16,21 +16,21 @@ function clearAllHistory() {
 
 <template>
   <div pt-2>
-    <div
+    <button
       text="blue-900 dark:blue-200"
       bg="blue-300 op-20 hover:(blue-800 op-20) dark:hover:(blue-200 op-20)"
       class="inline-flex items-center justify-center border border-transparent rounded-md px-4 py-2 text-sm font-medium focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
       @click="clearAllHistory"
     >
-      <div i-ri-eraser-line />
+      <span i-ri-eraser-line />
       <span class="ml-1">清空记录</span>
-    </div>
+    </button>
 
     <div flex="~ col">
       <div v-for="history in recipeHistories" :key="history.recipe.name" mt-2>
-        <StapleTag :active="false">
+        <DateTag>
           {{ dayjs(history.time).format('YYYY-MM-DD HH:mm:ss') }}
-        </StapleTag>
+        </DateTag>
         <DishTag :dish="history.recipe" />
       </div>
     </div>
