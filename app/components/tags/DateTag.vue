5e97da4731cfd87de94ea17535c381ceb223f3db
YunYouJun <me@yunyoujun.cn>
2025-01-24 11:27:43 +0800

    chore: upgrade deps & fix lint

--- app/components/tags/DateTag.vue
+++ app/components/tags/DateTag.vue
@@ -1,10 +1,10 @@
 <template>
   <div
-    class="rounded pureTag tagSize"
+    class="tagSize pureTag rounded"
     p="x-2"
     border="~ yellow-200 dark:yellow-800"
-    :bg="'yellow-300 opacity-20'"
-    :text="'yellow-800 dark:yellow-200'"
+    bg="yellow-300 opacity-20"
+    text="yellow-800 dark:yellow-200"
   >
     <slot />
   </div>
