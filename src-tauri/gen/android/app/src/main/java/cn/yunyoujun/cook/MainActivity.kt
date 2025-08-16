7b849b7e0af5fc00f2c04b07360d560ad5edc115
YunYouJun <me@yunyoujun.cn>
2025-08-17 07:08:06 +0800

    feat: add android init

---
+++ src-tauri/gen/android/app/src/main/java/cn/yunyoujun/cook/MainActivity.kt
@@ -0,0 +1,11 @@
+package cn.yunyoujun.cook
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
