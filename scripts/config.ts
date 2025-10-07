cf77e12220a92e87ecc693bbbff81f4dbeddd0b9
YunYouJun <me@yunyoujun.cn>
2025-10-07 18:29:47 +0800

    chore: compatible for nodejs dirname

--- scripts/config.ts
+++ scripts/config.ts
@@ -1,6 +1,7 @@
-import path from 'node:path'
+import path, { dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
 
-const __dirname = import.meta.dirname
+const __dirname = dirname(fileURLToPath(import.meta.url))
 const recipeCsvFile = path.resolve(__dirname, '../app/data/recipe.csv')
 const recipeJsonFile = path.resolve(__dirname, '../app/data/recipe.json')
 const incompatibleFoodsCsvFile = path.resolve(__dirname, '../app/data/incompatible-foods.csv')
